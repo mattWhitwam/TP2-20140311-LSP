a6dc6f56f7a39decce4630e76b2b704d1b31b3b7
211227307 <211227307@CPT-EN-113D01.ADS.CPUT.AC.ZA>
2014-03-11 17:42:39 +0200

    Created

---
+++ src/lspex/LSPEx.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lspex;
+
+/**
+ *
+ * @author 211227307
+ */
+public class LSPEx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
